a8f04046374c7219dfe8b5b14d81538ef865c049
Michaela Olson <mco44@cornell.edu>
2024-08-21 14:48:59 -0400

    add starting files

---
+++ src/app.py
@@ -0,0 +1,27 @@
+from dash import Dash, html
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+from datetime import datetime
+import geopy.distance
+from geopy.geocoders import Nominatim
+import plotly.express as px
+import plotly.graph_objects as go
+import random
+import matplotlib.colors as mcolors
+from matplotlib.patches import Patch
+from matplotlib.colors import LogNorm, Normalize
+
+from meteostat import Point, Daily, Hourly
+
+from datetime import datetime, timedelta
+
+
+app = Dash()
+
+app.layout = [html.Div(children='Hello World')]
+
+if __name__ == '__main__':
+    app.run(debug=True)
