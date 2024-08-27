7778422d7e17e5e772ec664aa12595f7ef9d49b5
Michaela Olson <mco44@cornell.edu>
2024-08-27 14:44:54 -0400

    allow dynamic switching of months

--- src/app.py
+++ src/app.py
@@ -19,6 +19,7 @@ import json
 import zipfile
 import requests
 import io
+import xml.etree.ElementTree as ET
 
 #from datetime import datetime, timedelta
 
@@ -55,21 +56,88 @@ city_pal = {"Boston":"#e6194B",
             "Newton":"#911eb4",
            }
 
+month_mapping = {
+    "01": "January", "02": "February", "03": "March", "04": "April",
+    "05": "May", "06": "June", "07": "July", "08": "August",
+    "09": "September", "10": "October", "11": "November", "12": "December"
+}
+
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
 # start mapbox
 maptoken = open(".mapbox").read()
 px.set_mapbox_access_token(maptoken)
 
+# functions for formatting
+def replacer(s, newstring, index, nofail=False):
+    # raise an error if index is outside of the string
+    if not nofail and index not in range(len(s)):
+        raise ValueError("index outside given string")
+    # if not erroring, but the index is still not in the correct range..
+    if index < 0:  # add it to the beginning
+        return newstring + s
+    if index > len(s):  # add it to the end
+        return s + newstring
+
+    # insert the new string between "slices" of the original
+    return s[:index] + newstring + s[index + 1:]
+
+
+def strtobr(string, every=25):
+    idxes = []
+    if len(string) < every:
+        return string
+    else:
+        for i in range(every, len(string), every):
+            nearest_space = string.find(" ", i)
+            if nearest_space > 0:
+                idxes.append(nearest_space)
+        for j in reversed(idxes):
+            string = replacer(string, "<br>", j)
+
+        return string
+
+def NormalizeData(data):
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
+
+def get_all_tripdata(s3url):
+    response = requests.get(s3url)
+    root = ET.fromstring(response.content)
+    s3files = []
+    for contents in root.findall('{http://s3.amazonaws.com/doc/2006-03-01/}Contents'):
+        key = contents.find('{http://s3.amazonaws.com/doc/2006-03-01/}Key').text
+        s3files.append(key)
+
+    tripfiles = {}
+    for file in s3files:
+        if "bluebikes-tripdata" in file:
+            year = file[0:4]
+            month = file[4:6]
+            tripfiles[month_mapping[month] + " " + str(year)] = s3url+"/"+file
+    return tripfiles
+
+def get_bike_data(s3path):
+    r = requests.get(s3path)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    for name in z.namelist():
+        if "__MACOSX" not in name:
+            csv_extract = name
+
+    return pd.read_csv(z.open(csv_extract), index_col = 0).dropna()
+
 # read and format data
-datapath = "https://s3.amazonaws.com/hubway-data/202407-bluebikes-tripdata.zip"
-r = requests.get(datapath)
-z = zipfile.ZipFile(io.BytesIO(r.content))
-for name in z.namelist():
-    if "__MACOSX" not in name:
-        csv_extract = name
+indexurl = "https://s3.amazonaws.com/hubway-data"
+tripdata = get_all_tripdata(indexurl)
+
+tripmonth = "May 2024"
+
 month = "202407"
-data_name = month + "-bluebikes-tripdata"
-#bike_data = pd.read_csv("../data/tripdata/" + data_name + "_cleaned.csv", index_col=0).dropna()
-bike_data = pd.read_csv(z.open(csv_extract), index_col = 0)
+bike_data = get_bike_data(tripdata[tripmonth])
 
 # get station locations based on averages
 station_locations = pd.read_csv("../data/geospacial_station_data_" + month + ".csv",
@@ -95,52 +163,6 @@ combined_station_data["Number of Rides Started"] = combined_station_data["Number
 #     combined_station_data["Number of Rides Started at " + daytime] = combined_station_data[
 #         "Number of Rides Started at " + daytime].fillna(0)
 
-# get the stations in order of most used
-most_used_order = combined_station_data.sort_values("Number of Rides Started", ascending=False).index
-
-
-# functions for formatting
-def replacer(s, newstring, index, nofail=False):
-    # raise an error if index is outside of the string
-    if not nofail and index not in range(len(s)):
-        raise ValueError("index outside given string")
-
-    # if not erroring, but the index is still not in the correct range..
-    if index < 0:  # add it to the beginning
-        return newstring + s
-    if index > len(s):  # add it to the end
-        return s + newstring
-
-    # insert the new string between "slices" of the original
-    return s[:index] + newstring + s[index + 1:]
-
-
-def strtobr(string, every=25):
-    idxes = []
-    if len(string) < every:
-        return string
-
-    else:
-        for i in range(every, len(string), every):
-            nearest_space = string.find(" ", i)
-            if nearest_space > 0:
-                idxes.append(nearest_space)
-
-        for j in reversed(idxes):
-            string = replacer(string, "<br>", j)
-
-        return string
-
-def NormalizeData(data):
-    return (data - np.min(data)) / (np.max(data) - np.min(data))
-
-styles = {
-    'pre': {
-        'border': 'thin lightgrey solid',
-        'overflowX': 'scroll'
-    }
-}
-
 df = combined_station_data.reset_index()[["index","Number of Rides Started"]].copy().sort_values("Number of Rides Started",
                                                                                                  ascending=False).rename(columns={"index":"Origin Station","Number of Rides Started":"trips"})
 #### set this as global so it can be updated elsewhere
@@ -153,23 +175,21 @@ dtable = dash_table.DataTable(
         'textAlign': 'left'
     }
 )
-#print(df)
 
 
 #### This works for rn
 app.layout = html.Div(
     [
-        html.H3("Bluebikes July 2024 data"),
+        html.H3("Bluebikes data"),
         html.P(
             "Click on a station to see all of the destinations from that station (min " + str(threshold) + " trips)"
         ),
-
-        # dcc.Dropdown(
-        #     id="type",
-        #     options=list(most_used_order),
-        #     value=most_used_order[0],
-        #     clearable=False,
-        # ),
+        dcc.Dropdown(
+            id="year-dropdown",
+            options=list(tripdata.keys()),
+            value=tripmonth,
+            clearable=False,
+        ),
    #     dcc.Graph(id="graph"),
         dcc.Graph(id="graph2"),
 
@@ -182,6 +202,34 @@ app.layout = html.Div(
 
     ]
 )
+
+# @app.callback(
+#     Output("graph2", "figure"),
+#     Input("year-dropdown", "value"),
+# )
+# def get_bike_month_data(dropval):
+#     bike_data = get_bike_data(tripdata[dropval])
+#     combined_station_data = station_locations.merge(station_data,
+#                                                     left_index=True, right_index=True, how="left")
+#     # combine city data w ride data
+#     station_to_city = combined_station_data.to_dict()["City"]
+#
+#     combined_station_data["Number of Rides Started"] = bike_data["start_station_name"].value_counts()
+#     # if there wasn't any rides started, it'll be nan -> fix
+#     combined_station_data["Number of Rides Started"] = combined_station_data["Number of Rides Started"].fillna(0)
+#
+#     fig = px.scatter_mapbox(combined_station_data.reset_index(),
+#                             lat="lat",
+#                             lon="lng",
+#                             hover_name="index",
+#                             color_discrete_map=city_pal,
+#                             color="City",
+#                             hover_data="index",
+#                             size="Number of Rides Started",
+#                             zoom=11,
+#                             height=800,
+#                             width=1000)
+#     return fig
 
 @app.callback(
     Output('click-data', 'children'),
@@ -221,8 +269,31 @@ def update_data_table(clickData):
 
 @app.callback(
     Output('graph2', 'figure'),
-    Input('graph2', 'clickData'))
-def display_bike_trips(clickData):
+    Input('graph2', 'clickData'),
+    Input("year-dropdown", "value"))
+def display_bike_trips(clickData, yearval):
+    global tripmonth
+    global bike_data
+    global combined_station_data
+
+    new_month = False
+
+    if tripmonth != yearval:
+        new_month = True
+        # means read in new data
+        tripmonth = yearval
+
+        bike_data = get_bike_data(tripdata[tripmonth])
+        combined_station_data = station_locations.merge(station_data,
+                                                        left_index=True, right_index=True, how="left")
+        # combine city data w ride data
+        station_to_city = combined_station_data.to_dict()["City"]
+
+        combined_station_data["Number of Rides Started"] = bike_data["start_station_name"].value_counts()
+        # if there wasn't any rides started, it'll be nan -> fix
+        combined_station_data["Number of Rides Started"] = combined_station_data["Number of Rides Started"].fillna(0)
+
+
     fig = px.scatter_mapbox(combined_station_data.reset_index(),
                             lat="lat",
                             lon="lng",
@@ -235,8 +306,7 @@ def display_bike_trips(clickData):
                             height=800,
                             width=1000)
 
-    if clickData is not None:
-
+    if clickData is not None and not new_month:
         if isinstance(clickData["points"][0]["customdata"], str):
             start_station = clickData["points"][0]["customdata"]
         else:
@@ -265,65 +335,6 @@ def display_bike_trips(clickData):
             )
     return fig
 
-
-
-# @app.callback(
-#     Output("graph", "figure"),
-#     Input("type", "value"),
-# )
-#
-# def generate_chart(target_station):
-#     ## read in data
-#     # current config to July 2024
-#
-#     fig = px.scatter_mapbox(combined_station_data.reset_index(),
-#                             lat="lat",
-#                             lon="lng",
-#                             hover_name="index",
-#                             color_discrete_map=city_pal,
-#                             color="City",
-#                             hover_data=["index", "Number of Rides Started"],
-#                             # color_continuous_scale=color_scale,
-#                             size="Number of Rides Started",
-#                             zoom=11,
-#                             height=800,
-#                             width=1000)
-#     # fig.update_traces(
-#     #     hovertemplate=None,
-#     #     hoverinfo='skip'
-#     # )
-#
-#     trips_to = bike_data.loc[bike_data["start_station_name"] == target_station]["end_station_name"].value_counts()
-#     norm_trip2 = NormalizeData(trips_to)
-#     cmap = colormaps["magma"]
-#     trips_to = trips_to.loc[trips_to > 8]
-#     start_station = target_station
-#     for trip in reversed(trips_to.index):
-#         trip_text = trip + " (" + str(trips_to[trip]) + " trips)"
-#         end_station = trip
-#         fig.add_trace(go.Scattermapbox(
-#             name=strtobr(trip_text),
-#             mode="lines",
-#             opacity=0.8,
-#             customdata=[start_station, end_station],
-#             lon=[combined_station_data.loc[start_station]["Long"], combined_station_data.loc[end_station]["Long"]],
-#             lat=[combined_station_data.loc[start_station]["Lat"], combined_station_data.loc[end_station]["Lat"]],
-#             hovertext=strtobr(trip_text),
-#             line={'width': np.floor(trips_to[trip] / 10 + 1),
-#                   "color": mcolors.rgb2hex(cmap(norm_trip2[trip]))},
-#             showlegend=False
-#             ),
-#
-#         )
-#
-#     # fig.update_layout(mapbox_style="open-street-map")
-#     #fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-#     # fig.update_traces(mode="lines", hovertemplate=None)
-#     # fig.update_layout(hovermode="x")
-#
-#     # fig.show()
-#
-#     return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
